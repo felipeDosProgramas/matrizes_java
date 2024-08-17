49af64f59d19174a71d2c4846576e6fd54e0e2b3
FelipeDosProgramas <felipeluizmsouza@gmail.com>
2024-08-17 00:50:17 -0300

    feat: added Equals Record's method;

--- src/Records/Matrix.java
+++ src/Records/Matrix.java
@@ -4,4 +4,16 @@ package Records;
 public record Matrix(
         double[][] rows
 ) {
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this)
+            return true;
+        if (!(obj instanceof Matrix))
+            return false;
+        for (int i = 0; i < 3; i++)
+            for (int j = 0; j < 3; j++)
+                if (this.rows[i][j] != ((Matrix) obj).rows()[i][j])
+                    return false;
+        return true;
+    }
 }
